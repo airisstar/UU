4949f682ba5e690db0aa8783c2159cdd0b818d5c
airisstar <mercinot4u@vk.com>
2024-12-14 20:58:59 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Anna': 2000, 'Lisa': 2003, 'Alex': 1989}
+print (my_dict)
+print (my_dict.get ('Anna'))
+print (my_dict.get ('ad'))
+my_dict.update ({'Sasha': 2009, 'Den': 2012})
+a = my_dict.pop ('Anna')
+print (a)
+print (my_dict)
+print ('Вторая часть задания')
+my_set = {1, 2, 1, 2, 1, 'sun', 'far', 'sun'}
+print (my_set)
+my_set.update ({23, 98})
+my_set.remove (1)
+print (my_set)
